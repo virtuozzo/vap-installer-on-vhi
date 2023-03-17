846dcbc985fb7df4d1b61a33bcc683ed3e507aea
sych74 <sych74@gmail.com>
2023-03-17 11:52:31 +0100

    Delete configureOnBeforeInit.js

--- scripts/configureOnBeforeInit.js
+++
@@ -1,4 +0,0 @@
-      var resp = {result:0};
-      var url = "https://raw.githubusercontent.com/sych74/vap-installer/ui/scripts/configure.yaml";
-      resp.settings = toNative(new org.yaml.snakeyaml.Yaml().load(new com.hivext.api.core.utils.Transport().get(url)));
-      return resp;
